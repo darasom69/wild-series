1c0e1af3e74da5cbe62085f7ccc69f397de69897
darasom69 <daha69@hotmail.fr>
2025-05-26 17:19:41 +0200

    quete 04

--- server/src/router.ts
+++ server/src/router.ts
@@ -14,17 +14,23 @@ router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
 /* ************************************************************************* */
+import programActions from "./modules/program/programActions";
 
+router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
+/* ************************************************************************* */
+import categoryActions from "./modules/category/categoryActions";
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
 // Declaration of a "Welcome" route
 
 import sayActions from "./modules/say/sayActions";
-
 router.get("/", sayActions.sayWelcome);
 
 /* ************************************************************************* */
 
-import programActions from "./modules/program/programActions";
-router.get("/api/programs", programActions.browse);
+
 
 
 
